a2ee1ff520f42b396d6a5f2a0c89bf05d3578ef0
JohannesVDM <johannes.321.vander@gmail.com>
2021-07-30 11:09:06 +0200

    personal changes

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  const titles = [
+    'Hello', 'Bye', 'Random', 'Titles', 'Are pretty sweet'
+  ]
+
+  let title = 'Random title';
+  const handleClick = () => {
+    console.log(title);
+    title = 'Hello';
+  }
+
+  return (
+    <React.Fragment>
+      <h2>{title}</h2>
+
+      <button type='button' className='btn'
+        onClick={handleClick}>Change Title</button>
+    </React.Fragment>
+  );
+
+
 };
 
 export default ErrorExample;
--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,31 @@
 import React, { useState } from 'react';
+//{{{{{{important}}}}}}
+// useState ~ function 
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  //should be invoked in function body
+  const [text, setText] = useState('First Title');
+
+  const clickHandler = () => {
+    if (text === 'First Title') {
+      setText('Second Title');
+    } else if (text === 'Second Title') {
+      setText('third Title');
+    } else if (text === 'third Title') {
+      setText('fourth Title');
+    } else if (text === 'fourth Title') {
+      setText('fifth Title');
+    } else if (text === 'fifth Title') {
+      setText('First Title');
+    }
+  }
+
+  return (
+    <React.Fragment>
+      <h2>{text}</h2>
+      <button type='button' onClick={clickHandler}>Change Title</button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
